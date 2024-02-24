17bef0c48ade36b833459cb04b2e8f442a1169c9
linbi <bhmail@foxmail.com>
2024-02-24 21:22:28 +0800

    init

---
+++ library.cpp
@@ -0,0 +1,11 @@
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "static hello test: \"Hello, World!\"" << std::endl;
+}
+
+int hello_add(int a,int b){
+    return (a+b);
+}
---
+++ library.h
@@ -0,0 +1,7 @@
+#ifndef C__LIB_LIBRARY_H
+#define C__LIB_LIBRARY_H
+
+void hello();
+int hello_add(int a,int b);
+
+#endif //C__LIB_LIBRARY_H
